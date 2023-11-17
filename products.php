8553ad7053440eee2c765f0bfc0d6235fbafd643
Your Name <you@example.com>
2023-11-17 23:17:44 +0100

    fix filter

--- products.php
+++ products.php
@@ -26,6 +26,12 @@ if ( isset($_POST['submit_checkbox'] )) {
     } else if ($_POST["quantity"] === "0") {
         $query ;
     } 
+     else if ($_POST["quantity"] === "3") {
+        $query .= "ORDER BY prix_unitaire ASC ";
+    } 
+    else if ($_POST["quantity"] === "4") {
+        $query .= "ORDER BY prix_unitaire DESC ";
+    } 
 
    
    
@@ -57,7 +63,12 @@ $result_all = $stmt->fetchAll();
             $query_category .= " ORDER BY quantite_min DESC";
         } else if ($_POST["quantity"] === "2") {
             $query_category .= " ORDER BY quantite_min ASC";
-        }
+        }   else if ($_POST["quantity"] === "3") {
+            $query_category .= " ORDER BY prix_unitaire ASC ";
+        } 
+        else if ($_POST["quantity"] === "4") {
+            $query_category .= " ORDER BY prix_unitaire DESC";
+        } 
         
       
      
@@ -71,7 +82,6 @@ $result_all = $stmt->fetchAll();
         $result = $result_all;
     }
 
-    $resultsPerPage = 5;
 
     
 
@@ -159,7 +169,18 @@ if (isset($_SESSION['name'])) {
             </div>
             <form method="post" action="#">
             <div class="grid-container">
-            <div class="item1 d-flex justify-content-end">
+
+            <div class="item1 d-flex justify-content-end align-items-center">
+                <span> Trier par : <span class="invisible">l</span> </span>
+                <select id="mySelect" name="quantity" class="form-select w-25" aria-label="Default select example" >
+                       
+                        <option <?php if (isset($_POST["quantity"]) && $_POST["quantity"] === "0"  ) {echo 'selected';}else {echo 'selected';}  ?>   value="0">All</option>
+                        <option <?php if (isset($_POST["quantity"]) && $_POST["quantity"] === "2"  ) echo 'selected'; ?>    value="2">Quantité, croissant </option>
+                        <option <?php if (isset($_POST["quantity"]) && $_POST["quantity"] === "1"  ) echo 'selected'; ?>    value="1">Quantité, décroissant </option>
+                        <option <?php if (isset($_POST["quantity"]) && $_POST["quantity"] === "3"  ) echo 'selected'; ?>    value="3">Prix, croissant </option>
+                        <option <?php if (isset($_POST["quantity"]) && $_POST["quantity"] === "4"  ) echo 'selected'; ?>    value="4">Prix, décroissant </option>
+
+                        </select>
     
   
 
@@ -173,34 +194,34 @@ if (isset($_SESSION['name'])) {
     <!-- Checkbox inputs -->
     <ul class="list-group">
     <li class="list-group-item">
-    <input id="firstCheckboxStretched" class="form-check-input me-1" type="checkbox" value="2" name="categories[]" <?php if (isset($categories) && in_array('2', $categories)) echo 'checked'; ?>>
-    <label class="form-check-label stretched-link" for="firstCheckboxStretched">Processeurs</label>
+    <input id="firstCheckboxStretched" class="form-check-input me-1 min" type="checkbox" value="2" name="categories[]" <?php if (isset($categories) && in_array('2', $categories)) echo 'checked'; ?>>
+    <label class="form-check-label stretched-link o1" for="firstCheckboxStretched">Processeurs</label>
     </li> 
     <li class="list-group-item">
-    <input id="secondCheckboxStretched" class="form-check-input me-1"  type="checkbox" value="3" name="categories[]" <?php if (isset($categories) && in_array('3', $categories)) echo 'checked'; ?>>
-    <label class="form-check-label stretched-link" for="secondCheckboxStretched">Cartes graphiques</label>
+    <input id="secondCheckboxStretched" class="form-check-input me-1 min"  type="checkbox" value="3" name="categories[]" <?php if (isset($categories) && in_array('3', $categories)) echo 'checked'; ?>>
+    <label class="form-check-label stretched-link o2" for="secondCheckboxStretched">Cartes graphiques</label>
     </li>
     
     
     <li class="list-group-item">
-    <input  id="thirdCheckboxStretched" class="form-check-input me-1"  type="checkbox" value="1" name="categories[]" <?php if (isset($categories) && in_array('1', $categories)) echo 'checked'; ?>>
+    <input  id="thirdCheckboxStretched" class="form-check-input me-1 min"  type="checkbox" value="1" name="categories[]" <?php if (isset($categories) && in_array('1', $categories)) echo 'checked'; ?>>
     <label class="form-check-label stretched-link" for="thirdCheckboxStretched">Boitiers PC</label>
     </li>
    <br>
     <h5>Quantité min</h5>
     <ul  class="list-group">
     <li class="list-group-item">
-    <input  id="thirdCheckboxStretched2" class="form-check-input me-1"  type="checkbox" value="4" name="quantite_min" <?php if (isset($_POST["quantite_min"]) && $_POST["quantite_min"] === "4" ) echo 'checked'; ?> >
+    <input  id="thirdCheckboxStretched2" class="form-check-input me-1 min"  type="checkbox" value="4" name="quantite_min" <?php if (isset($_POST["quantite_min"]) && $_POST["quantite_min"] === "4" ) echo 'checked'; ?> >
     <label class="form-check-label stretched-link" for="thirdCheckboxStretched2">Quantite min</label>
     </li>
     </ul>
     </ul>
     <br>
-     <h5>Quantité</h5>
+     <!-- <h5>Quantité</h5> -->
     <!-- Checkbox inputs -->
-    <ul class="list-group">
+    <!-- <ul class="list-group">
     <li class="list-group-item">
-    <input id="firstquantity0" class="form-check-input me-1" type="radio" value="0" name="quantity" <?php if (isset($_POST["quantity"]) && $_POST["quantity"] === "0" || !isset($_POST["quantity"]) ) echo 'checked'; ?> >
+    <input id="firstquantity0"  class="form-check-input me-1 " type="radio" value="0" name="quantity" <?php if (isset($_POST["quantity"]) && $_POST["quantity"] === "0" || !isset($_POST["quantity"]) ) echo 'checked'; ?> >
     <label class="form-check-label stretched-link" for="firstquantity0">All</label>
 
     </li> 
@@ -210,15 +231,15 @@ if (isset($_SESSION['name'])) {
 
     </li> 
     <li class="list-group-item">
-    <input id="firstquantity2" class="form-check-input me-1" type="radio" value="2" name="quantity" <?php if (isset($_POST["quantity"]) && $_POST["quantity"] === "2" ) echo 'checked'; ?> >
+    <input id="firstquantity2" class="form-check-input me-1 " type="radio" value="2" name="quantity" <?php if (isset($_POST["quantity"]) && $_POST["quantity"] === "2" ) echo 'checked'; ?> >
     <label class="form-check-label stretched-link" for="firstquantity2">Quantité, croissant </label>
 
     </li> 
-    </ul>
+    </ul> -->
     
   
     <br>
-    <button type="submit" name="submit_checkbox" class="btn btn-info btn-lg">
+    <button id="bu" type="submit" name="submit_checkbox" class="btn btn-info btn-lg invisible">
           <span class="glyphicon glyphicon-filter"><i class="fas fa-filter"></i></span> Filter </button>
 
 </form>
@@ -306,7 +327,72 @@ if (isset($_SESSION['name'])) {
 <?php include 'layout/js.php' ; ?>
 
 
+<script>
+    // start Quantity
 
+// const radioButtons = document.getElementsByName("quantity");
+
+// radioButtons.forEach((radio) => {
+//   radio.addEventListener("change", () => {
+//     let selectedValue = "";
+
+//     radioButtons.forEach((rb) => {
+//       if (rb.checked) {
+
+//         selectedValue = rb.value;
+//       }
+//     });
+
+
+//      if (selectedValue !== null ) {
+//         document.getElementById("bu").click();
+//      }
+
+//   });
+// });
+    // end Quantity
+
+
+// Get the select element by its ID
+const selectElement = document.getElementById("mySelect");
+
+// Add change event listener to the select element
+selectElement.addEventListener("change", function() {
+  // Get the selected value
+  const selectedValue = this.value;
+ console.log(selectedValue)
+  // Display the selected value
+  if (selectedValue !== null ) {
+        document.getElementById("bu").click();
+     }
+    });
+
+
+
+// Get all checkbox elements with the class 'option'
+const checkboxes = document.querySelectorAll('.min');
+
+// Add change event listeners to all checkboxes
+checkboxes.forEach(checkbox => {
+  checkbox.addEventListener('change', function() {
+    let selectedValues = [];
+    
+    checkboxes.forEach(cb => {
+      if (cb.checked) {
+        selectedValues.push(cb.value);
+      }
+    });
+
+    if (selectedValues.length > 0) {
+        document.getElementById("bu").click();     } else {
+            document.getElementById("bu").click();     }
+  });
+});
+
+
+
+    
+</script>
 
 
 
